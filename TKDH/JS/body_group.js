ab281bcb905f6a0578d05a50ad3c0295619bf8ea
QuangLT <81897940+TueThuEnglish@users.noreply.github.com>
2021-04-08 20:20:20 +0700

    upload

---
+++ TKDH/JS/body_group.js
@@ -0,0 +1,22 @@
+$('.slide-body').flickity({
+    // options
+    cellAlign: 'left',
+    contain: true,
+    wrapAround:true,
+    autoPlay:5000,
+  });
+  $('.lecturects-group').flickity({
+    // options
+    cellAlign: 'left',
+    contain: true,
+    wrapAround:true,
+    autoPlay:5000,
+  });
+  $('.students-group').flickity({
+    // options
+    cellAlign: 'left',
+    contain: true,
+    wrapAround:true,
+    autoPlay:5000,
+  });
+  
